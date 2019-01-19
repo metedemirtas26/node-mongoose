f4d750414a1cb75e2496d80d448b4eaa405b3b28
metedemirtas26 <metedemirtas26@gmail.com>
2019-01-19 16:37:56 +0100

    Mongoose Part 2

--- index.js
+++ index.js
@@ -9,6 +9,29 @@ connect.then((db) => {
 
     console.log('Connected correctly to server');
 
+    Dishes.create({
+        name: 'Uthapizza',
+        description: 'Test'
+    })
+        .then((dish) => {
+            console.log(dish);
+
+            return Dishes.find({}).exec();
+        })
+        .then((dishes) => {
+            console.log(dishes);
+
+            return Dishes.remove({});
+        })
+        .then(() => {
+            return mongoose.connection.close();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
+
+    /*
     var newDish = Dishes({
         name: 'Uthappizza',
         description: 'test'
@@ -30,6 +53,6 @@ connect.then((db) => {
         })
         .catch((err) => {
             console.log(err);
-        });
+        }); */
 
 });
